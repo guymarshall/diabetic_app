7270696168a4248adb1b01671689765ad198985c
Guy Marshall <guymarshall9@gmail.com>
2022-05-14 16:14:24 +0100

    Make user.py class usable

--- user.py
+++ user.py
@@ -1,18 +1,12 @@
-class Employee:
-    """A sample Employee class"""
-
-    def __init__(self, last, first, pay):#last and first the wrong way around
-        self.first = first
-        self.last = last
-        self.pay = pay
-    
-    @property
-    def email(self):
-        return f"{self.first}.{self.last}@email.com"
+class User:
+    def __init__(self, first_name, last_name, birthdate):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthdate = birthdate
     
     @property
     def fullname(self):
-        return f"{self.first} {self.last}"
+        return f"{self.first_name} {self.last_name}"
     
     def __repr__(self):
-        return f"Employee('{self.first}', '{self.last}', '{self.pay}')"
+        return f"User('{self.first_name}', '{self.last_name}', '{self.birthdate}')"
