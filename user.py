a7c0b7e74d8f080b16b2dd56a567e3ef1b116db9
Guy Marshall <guymarshall.uk@gmail.com>
2022-05-24 13:56:57 +0100

    Convert user class to dataclass + add functionality

--- user.py
+++ user.py
@@ -1,12 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True, order=True)
 class User:
-    def __init__(self, first_name, last_name, birthdate):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birthdate = birthdate
+    user_id: int
+    first_name: str
+    last_name: str
+    sex: str
+
+    def get_all():
+        return ''
     
-    @property
-    def fullname(self):
-        return f"{self.first_name} {self.last_name}"
+    def get_one(user_id):
+        return ''
     
-    def __repr__(self):
-        return f"User('{self.first_name}', '{self.last_name}', '{self.birthdate}')"
+    def insert(user_id):
+        return '';
