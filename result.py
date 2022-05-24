b1dc74c89587ae238fb656a8506002cafa6a8cb0
Guy Marshall <guymarshall.uk@gmail.com>
2022-05-24 13:57:22 +0100

    Add functionality to result class

--- result.py
+++ result.py
@@ -1,6 +1,6 @@
 class Result:
-    def __init__(self, user, time, date, blood_sugar, carbs, insulin_short, insulin_long, insulin_correction, notes):
-        self.user = user
+    def __init__(self, user_id, time, date, blood_sugar, carbs, insulin_short, insulin_long, insulin_correction, notes):
+        self.user_id = user_id
         self.time = time
         self.date = date
         self.blood_sugar = blood_sugar
@@ -9,3 +9,12 @@ class Result:
         self.insulin_long = insulin_long
         self.insulin_correction = insulin_correction
         self.notes = notes
+    
+    def get_all():
+        return ''
+    
+    def get_one(result_id):
+        return ''
+    
+    def insert(result_data):
+        return '';
